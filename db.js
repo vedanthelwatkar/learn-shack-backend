a4488719bdc8101d53bb819e1e7f6ae93af7eda1
Vedant Helwatkar <vedanthelwatkar@rathi.com>
2025-04-12 18:15:18 +0530

    trim added

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST?.trim(),
+  user: process.env.DB_USER?.trim(),
+  password: process.env.DB_PASSWORD?.trim(),
+  database: process.env.DB_NAME?.trim(),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
