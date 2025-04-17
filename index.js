6d56bb66c113451c2486c018c995b731e1919876
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-17 19:42:17 +0530

    origin changed in cors

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://learnshackedu.com",
+    "https://learn-shack.vercel.app" ],
+}));
 
 // Middleware
 app.use(express.json());
