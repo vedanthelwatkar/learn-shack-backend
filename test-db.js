17eb62bbe5a7625406a4bf2803872683d1efb9f2
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-22 21:46:12 +0530

    utils deleted

--- test-db.js
+++
@@ -1,8 +0,0 @@
-import { db } from "./db.js";
-
-try {
-  const [result] = await db.execute("SELECT 1");
-  console.log("✅ DB Connected");
-} catch (err) {
-  console.error("❌ DB Connection failed:", err.message);
-}
